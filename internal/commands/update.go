35110a5c715cb49a4b39113866f1d6cf29af1499
Dev Com Biscoito <devcombiscoito@gmail.com>
2026-02-05 14:59:48 -0300

    feat: add automatic git pull to s update command

--- internal/commands/update.go
+++ internal/commands/update.go
@@ -1,16 +1,23 @@
 package commands
 
 import (
-	"os/exec"
+	"fmt"
 	"os"
+	"os/exec"
 )
 
 func RodarUpdate() {
 	directory := "/usr/local/bin"
-	
+
+	fmt.Println("==> Buscando atualizações no GitHub...")
+	execPull := exec.Command("git", "pull", "main", "master")
+	execPull.Stdout = os.Stdout
+	execPull.Stderr = os.Stderr
+	execPull.Run()
+
+	fmt.Println("==> Compilando nova versão...")
 	execUpdate := exec.Command("go", "build", "-o", "s", "./cmd/s")
 	execUpdate.Run()
-
 
 	execMove := exec.Command("sudo", "mv", "s", directory)
 	execMove.Stdin = os.Stdin
